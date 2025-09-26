38ec2bc92e34a78af25d27d0460bdacc03c0e22d
Mcenroe Ryan <mcenroe@cbcinc.ai>
2025-09-26 17:29:59 -0500

    #Production Ready Code [09/26/2025]

--- src/screens/DemandPlanning.jsx
+++ src/screens/DemandPlanning.jsx
@@ -46,8 +46,7 @@ import ChatBot from "./components/Chatbox";
 import { AnalyticsFrameSection } from "./components/AnalyticsFrameSection";
 import ScenarioSection from "./components/ScenarioSection";
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL;
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 function getSelectedNames(selectedIds, options, optionKey, displayKey) {
   return options
